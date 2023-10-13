818e7aa3e822de51866025c2a75d51964945bb48
sarfraz-ali-con <sarfraz.ali.con@gmail.com>
2023-10-13 22:26:20 +0500

    deleted welcome file and now added home file in view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <div id="form">
+            <form action="">
+                <input type="text" name="user-name" placeholder="Enter your Name">
+                <input type="email" name="user-email" placeholder="Enter your Email">
+                <input type="password" name="user-password" placeholder="Enter your Password">
+                <button type="submit">Create</button>
+            </form>
+        </div>
+        <div id="data">
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        
+                    </tr>
+                </thead>
+
+            </table>
+        </div>
+    </main>
+</body>
+</html>

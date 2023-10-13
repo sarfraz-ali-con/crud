e5717b2279a603dd1077cfef2bd49779d44758ba
sarfraz-ali-con <sarfraz.ali.con@gmail.com>
2023-10-13 22:59:36 +0500

    updated home file with css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <header>
-
+    <header class="container">
+        <h1 class="text-center">CRUD Header</h1>
     </header>
-    <main>
-        <div id="form">
-            <form action="">
-                <input type="text" name="user-name" placeholder="Enter your Name">
-                <input type="email" name="user-email" placeholder="Enter your Email">
-                <input type="password" name="user-password" placeholder="Enter your Password">
-                <button type="submit">Create</button>
-            </form>
-        </div>
-        <div id="data">
-            <table>
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        
-                    </tr>
-                </thead>
-
-            </table>
+    <main class="container">
+        <div id="form" class="row">
+            <div class="col-md-6">
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="user-name" placeholder="Enter your Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" name="user-email" placeholder="Enter your Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="user-password" placeholder="Enter your Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary"><span style="font-size: 18px;font-weight:bold;">+</span> Create</button>
+                </form>
+            </div>
+            <div id="data" class="col-md-6">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Sarfraz</td>
+                            <td>sarfraz@gmail.com</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </main>
 </body>
